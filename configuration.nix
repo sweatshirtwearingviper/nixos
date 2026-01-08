80f37969e60ba75c6051f0c3f387303648313c5f
thesweateredviper@gmail.com <thesweateredviper@gmail.com>
2026-01-07 19:27:57 -0700

    33 initial commit

---
+++ configuration.nix
@@ -0,0 +1,153 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+# Home manager for theming and config, npins for catpuccin themes
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Define home manager settings
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.backupFileExtension = "backup";
+
+# Import the ./home.nix file for home manager, and npins catpuccin for themes
+  home-manager.users.sweaty = {
+    imports = [
+      ./home.nix
+    ];
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos-envy"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openconnect
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sweaty = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  hardware.graphics.enable = true;
+
+  # This should be enabled here, to make certain system-level changes, such as a adding a desktop session entry
+  programs.hyprland.enable = true;
+  
+  # Allow unfree packages if necessary.
+  # nixpkgs.config.allowUnfree = true;
+
+  # Needed for home-manager to set configurations correctly
+  environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
+  
+  # System-wide packages (i.e. root programs)
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
+
---
+++ home.nix
@@ -0,0 +1,120 @@
+{config, pkgs, lib, ...}:
+
+let
+  homeDirectory = "/home/sweaty";
+in
+{
+  home.username = "sweaty";
+  home.homeDirectory = homeDirectory;
+  home.stateVersion = "25.11";
+
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nix btw";
+      nrs = "sudo nixos-rebuild switch";
+      nec = "sudo vim /etc/nixos/configuration.nix";
+      neh = "sudo vim /etc/nixos/home.nix";
+    };
+  };
+
+  # Hyprland settings
+  programs.kitty.enable = true;
+  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.settings = {
+
+    ### LOOK AND FEEL ###
+    general = {
+      gaps_in = 0;
+      gaps_out = 0;
+      border_size = 0;
+      resize_on_border = false;
+      allow_tearing = false;
+      layout = "dwindle";
+    };
+
+    dwindle = {
+      pseudotile = true;
+      preserve_split = true;
+    };
+
+    ### INPUT ###
+    # Variables for keybinds
+    "$mod" = "SUPER";
+    "$terminal" = "kitty";
+    "$fileManager" = "dolphin";
+    "$menu" = "fuzzel";
+
+    # Key binds
+    bind = 
+      [
+        "$mod, N, exec, $menu"
+        "$mod, T, exec, $terminal"
+        "$mod, C, killactive"
+        "$mod, F, fullscreen"
+        "$mod, V, togglefloating,"
+      ]
+
+    # Some insane Nix programming that iterates over workspace binds
+      ++
+      (builtins.concatLists (builtins.genList (i:
+        let ws = i + 1;
+        in [
+          "$mod, code:1${toString i}, workspace, ${toString ws}"
+          "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+        ])9));
+
+    # Mouse binds
+    bindm = 
+      [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+    # Function key binds for laptop
+    bindel = 
+      [
+        ",XF86AudioRaiseVolume, exec, wpctl set_volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set_volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ];
+  };
+  
+
+  home.packages = with pkgs; [
+    # Terminal programs
+    btop
+    acpi
+    fastfetch
+    git
+
+    # Wayland accesories
+    fuzzel
+    dunst
+    libnotify
+    swww
+    hyprshot
+
+    # QT applications
+    kdePackages.dolphin
+    kdePackages.qt6ct
+
+    # GTK applications
+    firefox
+    remmina
+
+    # Standalone applications
+    godot
+    zed-editor-fhs
+
+    # Electron app
+  ];
+
+  # home.activation = {
+  #   source-bashrc = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #     run source /home/sweaty/.bashrc
+  #   '';
+  # };
+}
